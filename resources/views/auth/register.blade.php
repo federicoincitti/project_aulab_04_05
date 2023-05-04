c0740ad3fecee433de2e1beb12d7f361c0b12a09
Andrea Lanciano <andrealanciano@me.com>
2023-05-04 16:16:46 +0200

    creato rotte get per form registrazione e login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,12 @@
-<h1>bye</h1>
+<x-layout>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <h1>Welcome per la prima volta nel tuo sito di viaggi preferito!</h1>
+            </div>
+        </div>
+    </div>
+
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+
+Route::get('/login', [UserController::class, 'showLogin'])->name('user.login');
+
+Route::get('/register', [UserController::class, 'showRegister'])->name('user.register');
