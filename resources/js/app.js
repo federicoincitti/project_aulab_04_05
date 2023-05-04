565460c5420e34e36bde96d40f9e4f67e2813c82
federicoincitti <federico.incitti1@gmail.com>
2023-05-04 15:58:39 +0200

    fortify installato

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
 import './bootstrap';
 
 import '/node_modules/bootstrap/dist/js/bootstrap.min.js';
+
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,15 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? '' }}</title>
-   
+  
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
-    
+    <x-navbar />
 
     {{ $slot }}
-    
+
+    <div class="container vh-100"> 
+    </div>
+
+    <x-footer />
 </body>
 </html>
